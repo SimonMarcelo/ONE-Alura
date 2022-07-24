9f3310a05c8b8aa9d6726f333149f29c121d6df5
SimonMarcelo <marcelo.simon@gmail.com>
2022-07-24 18:20:33 -0300

    Pagina terminada

--- JS en la WEB - Manipulando el DOM con JS/script.js
+++ JS en la WEB - Manipulando el DOM con JS/script.js
@@ -1,4 +1,7 @@
-( () => { const btn = document.querySelector("[data-form-btn]");
+import checkComplete from "./components/checkComplete.js";
+import deleteIcon from "./components/deleteIcon.js";
+
+const btn = document.querySelector("[data-form-btn]");
 
 const createTask = (evento) => {
     evento.preventDefault();
@@ -15,30 +18,14 @@ const createTask = (evento) => {
     titleTask.classList.add("task");
     titleTask.innerText = value;
     taskContent.appendChild(titleTask);
-    const content = `<i class="fas fa-trash-alt trashIcon icon"></i>`
+    // taskContent.appendChild(deleteIcon());
+    // const content = `<i class="fas fa-trash-alt trashIcon icon"></i>`
     // task.innerHTML = content;
     task.appendChild(taskContent);
-
+    task.appendChild(deleteIcon())
     list.appendChild(task);
     console.log(content);
 };
 
 //Arrow functions o funciones anónimas
 btn.addEventListener("click", createTask);
-
-const checkComplete = ()=> {
-    const i = document.createElement("i");
-    i.classList.add("far" , "fa-check-square" , "icon");
-    i.addEventListener("click",completeTask)
-    return i
-}
-
-const completeTask = (event) => {
-    const element = event.target;
-    element.classList.toggle("fas");
-    element.classList.toggle("completeIcon");
-    element.classList.toggle("far");
-    console.log("Complete Task");
-};
-})();
-
